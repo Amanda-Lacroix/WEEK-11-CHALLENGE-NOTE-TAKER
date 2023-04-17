5a39532f441e2e316559db7b25cef12ae412eee6
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-04-17 15:06:16 -0300

    added code to server.js to provide the path for the index.html and notes.html routes

--- server.js
+++ server.js
@@ -2,9 +2,18 @@
 const express = require('express');
 const app = express ();
 const PORT = 3001;
+const path = require('path');
 
 // Middleware that defaults to serve the files in the Public folder
 app.use(express.static('public'));
 
+// The GET Express.js routes for the index.html & notes.html
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/index', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/index.html')));
   
-  app.listen(PORT)
+app.listen(PORT, () => {
+console.log ('Test-it is running')
+});
