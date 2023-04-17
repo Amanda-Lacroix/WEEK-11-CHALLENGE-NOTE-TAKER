c60fe5cb22c449317182ad27fea21d99ea914c0a
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-04-17 15:58:38 -0300

    revised file structure, amended code in server.js

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 // To use Express.js
 const express = require('express');
-const app = express ();
-const PORT = 3001;
 const path = require('path');
+const app = express ();
+const PORT = 3000;
+
 
 // Middleware that defaults to serve the files in the Public folder
 app.use(express.static('public'));
 
+app.get('/', (req, res) => res.send('Go to /index or /notes'));
+
 // The GET Express.js routes for the index.html & notes.html
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.get('/index', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/notes.html')));
   
 app.listen(PORT, () => {
-console.log ('Test-it is running')
+console.log ('Test-running on 3000')
 });
